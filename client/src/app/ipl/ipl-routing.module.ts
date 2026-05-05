1183bb29b4f878700ad9850379e5737261c5061e
Bisht-123 <dummy@gmail.com>
2026-05-05 09:57:19 +0000

    day23

--- client/src/app/ipl/ipl-routing.module.ts
+++ client/src/app/ipl/ipl-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { TeamCreateComponent } from "./components/teamcreate/teamcreate.component";
+import { CricketerArrayComponent } from "./components/cricketerarray/cricketerarray.component";
+import { CricketerCreateComponent } from "./components/cricketercreate/cricketercreate.component";
+import { MatchCreateComponent } from "./components/matchcreate/matchcreate.component";
+import { VoteComponent } from "./components/vote/vote.component";
+import { TicketBookingComponent } from "./components/ticketbooking/ticketbooking.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "dashboard", pathMatch: "full" },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "teamcreate", component: TeamCreateComponent },
+  { path: "cricketerarray", component: CricketerArrayComponent },
+  { path: "cricketercreate", component: CricketerCreateComponent },
+  { path: "matchcreate", component: MatchCreateComponent },
+  { path: "vote", component: VoteComponent },
+  { path: "ticketbooking", component: TicketBookingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class IplRoutingModule {}
--- client/src/app/ipl/ipl.module.ts
+++ client/src/app/ipl/ipl.module.ts
@@ -5,15 +5,38 @@ import { IplRoutingModule } from "./ipl-routing.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
+import { TeamCreateComponent } from "./components/teamcreate/teamcreate.component";
+import { CricketerArrayComponent } from "./components/cricketerarray/cricketerarray.component";
+import { CricketerCreateComponent } from "./components/cricketercreate/cricketercreate.component";
+import { MatchCreateComponent } from "./components/matchcreate/matchcreate.component";
+import { VoteComponent } from "./components/vote/vote.component";
+import { TicketBookingComponent } from "./components/ticketbooking/ticketbooking.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    TeamCreateComponent,
+    CricketerArrayComponent,
+    CricketerCreateComponent,
+    MatchCreateComponent,
+    VoteComponent,
+    TicketBookingComponent
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     HttpClientModule,
+    IplRoutingModule
   ],
   exports: [
-    
+    DashboardComponent,
+    TeamCreateComponent,
+    CricketerArrayComponent,
+    CricketerCreateComponent,
+    MatchCreateComponent,
+    VoteComponent,
+    TicketBookingComponent
   ]
 })
 export class IplModule {}
